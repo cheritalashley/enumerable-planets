331efbff3102130b79ad81ad7f3af36fa148e977
Cherita <lashleynook@gmail.com>
2020-10-05 18:30:18 -0500

    completed chapter 14 exercises

--- src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
+++ src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
@@ -1,13 +1,22 @@
 package org.launchcode.enumerableplanets.controllers;
 
+import org.launchcode.enumerableplanets.data.Planets;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PlanetController {
 
     @GetMapping
-    public String displayIndex() {
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
         return "index";
+    }
+
+    @GetMapping("details")
+    public String displayPlanetInfo(Model model) {
+        model.addAttribute("planets", Planets.values());
+        return "planetDetails";
     }
 }
